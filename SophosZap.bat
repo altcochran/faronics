45686f7dcd8812b623e8f4a0bbb8309cb6627e3d
altcochran <91136254+altcochran@users.noreply.github.com>
2022-08-16 13:24:10 -0400

    Update SophosZap.bat

--- SophosZap.bat
+++ SophosZap.bat
@@ -1,5 +1,5 @@
 md c:\temp
 
-xcopy \\utlsoftdeploy\installs\Sophos\SophosZap.exe C:\temp\
+xcopy \\utlsoftdeploy\installs\Sophos\SophosZap.exe C:\temp\ /y
 
 start "c:\temp\SophosZap.exe" --confirm /s
