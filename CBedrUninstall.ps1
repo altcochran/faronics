89f06b5e7c36a7c67e721981e0ae87ebf9baf3bd
altcochran <91136254+altcochran@users.noreply.github.com>
2022-08-23 09:58:22 -0400

    Update CBedrUninstall.ps1

--- CBedrUninstall.ps1
+++ CBedrUninstall.ps1
@@ -1,2 +1,2 @@
-C:\Windows\System32\takeown.exe /F /R "C:\Windows\CarbonBlack\"
+C:\Windows\System32\takeown.exe /F "C:\Windows\CarbonBlack\" /R
 Invoke-Expression -Command "C:\Windows\CarbonBlack\uninst.exe /S"
